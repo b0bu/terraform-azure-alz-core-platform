461fc71b47f988df7a30fc4a43df5d6e1f7c7ac5
Jason Leitch <jasonleitch0@gmail.com>
2022-12-06 14:48:34 +0000

    chore: add backend

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = ">= 3.0.2"
     }
   }
+  backend "azurerm" {
+    resoure_group_name = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name = var.container_name
+    key = var.key    
+  }
 }
