d880c59a45ab0ffe32cc77cf059dd8719d47989c
Jason Leitch <jasonleitch0@gmail.com>
2022-12-19 18:28:18 +0000

    chore: add outputs

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,11 @@ output "managed_identity_principal_ids" {
 output "managed_identity_policy_assignment_roles" {
   value = local.managed_identity_policy_assignment_roles
 }
+
+# output "subscription_billing_scope" {
+#   value = data.azurerm_billing_enrollment_account_scope.ea.id
+# }
+
+output "account_id" {
+  value = data.azurerm_client_config.core.client_id
+}
